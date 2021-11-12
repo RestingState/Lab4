58fa708e805cda748b7c235464a9734e9243dea6
RestingState <84774115+RestingState@users.noreply.github.com>
2021-11-12 09:02:46 +0200

    Updated model

--- database_model.py
+++ database_model.py
@@ -3,14 +3,14 @@ from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
 import os
 
 # CHANGE THIS SETTINGS IF YOU HAVE ANY DIFFERENT
-DB_SCHEME = 'postgresql+psycopg2'
-DB_USERNAME = 'postgres'
-DB_PASSWORD = 'test'
+DB_SCHEME = 'mysql+pymysql'
+DB_USERNAME = 'root'
+DB_PASSWORD = '12345'
 DB_SERVER = 'localhost'
-DB_PORT = '5432'
+DB_PORT = '3306'
 
 # Not necessary to change
-DB_NAME = 'denys_project'
+DB_NAME = 'student_rating'
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 connection_string = '{}://{}:{}@{}:{}/{}'.format(
@@ -26,36 +26,36 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'users'
-    id = Column(Integer(), autoincrement=True, primary_key=True, unique=True)
+    id = Column(Integer(), autoincrement=True, primary_key=True)
     full_name = Column(String(64), nullable=False)
     login = Column(String(24), nullable=False, unique=True)
-    password = Column(String(48), nullable=False)
+    password = Column(String(80), nullable=False)
     email = Column(String(80), nullable=False, unique=True)
 
 class Major(Base):
     __tablename__ = 'major'
-    id = Column(Integer(), autoincrement=True, primary_key=True, unique=True)
+    id = Column(Integer(), autoincrement=True, primary_key=True)
     name = Column(String(80), nullable=False, unique=True)
 
 class Subject(Base):
     __tablename__ = 'subject'
-    id = Column(Integer(), autoincrement=True, primary_key=True, unique=True)
+    id = Column(Integer(), autoincrement=True, primary_key=True)
     name = Column(String(80), nullable=False, unique=True)
 
 class Student(Base):
     __tablename__ = 'student'
-    id = Column(Integer(), autoincrement=True, primary_key=True, unique=True)
+    id = Column(Integer(), autoincrement=True, primary_key=True)
     name = Column(String(64), nullable=False)
-    major_id = Column(Integer(), ForeignKey('major.id'))
+    major_id = Column(Integer(), ForeignKey('major.id'), nullable=False)
     major = relationship('Major', backref='students')
     rating = Column(Integer(), nullable=False)
     
     
 class Mark(Base):
     __tablename__ = 'mark'
-    id = Column(Integer(), autoincrement=True, primary_key=True, unique=True)
+    id = Column(Integer(), autoincrement=True, primary_key=True)
     student_id = Column(Integer(), ForeignKey('student.id'))
     subject_id = Column(Integer(), ForeignKey('subject.id'))
     student = relationship('Student', backref='marks')
     subject = relationship('Subject', backref='marks')
-    grade = Column(Integer, nullable=False)
+    grade = Column(Integer(), nullable=False)
